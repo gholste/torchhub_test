156f12dfc678c95b90ef0977ac890e9729facfd3
gholste <gholste@utexas.edu>
2024-08-28 19:08:01 -0400

    First commit.

---
+++ hubconf.py
@@ -0,0 +1,11 @@
+import torchvision
+
+def test_model(pretrained=False):
+    model = torchvision.models.convnext_tiny(pretrained=False)
+
+    if pretrained:
+        weights = torch.hub.load_state_dict_from_url('https://github.com/gholste/torchhub_test/releases/download/v1.0/weights.pt', progress=False)['weights']
+        msg = model.load_state_dict(weights, strict=True)
+        print(msg)
+
+    return model
